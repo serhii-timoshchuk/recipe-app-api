18ac5d3b505fad49168a6502926e02176dfef312
serhii timoshchuk <thegandalfgreat@gmail.com>
2022-07-11 16:11:06 +0300

    Django +postgresql

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+'''
+Calculator function
+'''
+
+
+def add(x, y):
+    '''Add and return result'''
+    return x+y
+
+def substract(x, y):
+    '''Substract x from y and return result'''
+    return y - x
+
---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+'''
+Sample tests
+'''
+from django.test import SimpleTestCase
+
+from . import calc
+
+class CalcTest(SimpleTestCase):
+    """Test the calc modul"""
+    def test_add(self):
+        '''TEst adding numbers together '''
+        result = calc.add(5,6)
+        self.assertEqual(result,11)
+
+    def test_substract_number(self):
+        '''Test substarcitng numbers'''
+        res = calc.substract(10,15)
+
+        self.assertEqual(res,5)
