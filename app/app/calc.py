ad9b74e90a2343c0a6756ba220d9bbe0691310c2
serhii timoshchuk <thegandalfgreat@gmail.com>
2022-07-11 16:22:03 +0300

    fixed pep8

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,6 +7,7 @@ def add(x, y):
     '''Add and return result'''
     return x+y
 
+
 def substract(x, y):
     '''Substract x from y and return result'''
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -5,6 +5,7 @@ from django.test import SimpleTestCase
 
 from . import calc
 
+
 class CalcTest(SimpleTestCase):
     """Test the calc modul"""
     def test_add(self):
